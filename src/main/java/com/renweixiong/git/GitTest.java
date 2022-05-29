fea0ee8c618e1e3644ad55b6f6f4d384c2d40073
renweixiong <renweixiong@qq.com>
2022-05-29 11:02:08 +0800

    first commit

---
+++ src/main/java/com/renweixiong/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.renweixiong.git;
+
+/**
+ * @author 任伟雄
+ * @create 2022-05-29-10:57
+ */
+
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
